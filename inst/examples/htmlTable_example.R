17c035ba72f7636cf4f855dd7ae792f1b8d478a3
Max Gordon <max@gforge.se>
2014-07-27 00:00:00 +0000

    version 0.6.7

--- inst/examples/htmlTable_example.R
+++ inst/examples/htmlTable_example.R
@@ -1,14 +1,16 @@
 # A simple example
 # note that this won't show due to the second example
-mx <- matrix(1:6, ncol=3) 
-rownames(mx) <- LETTERS[1:NROW(mx)] 
+mx <- matrix(1:6, ncol=3)
+rownames(mx) <- LETTERS[1:NROW(mx)]
 colnames(mx) <- sprintf("Col %s", LETTERS[1:NCOL(mx)])
 
 # If you just want to output your matrix, this is all you need
-htmlTable(mx)
+# We also force the output to be printed to std_out instead of th
+# graphical viewer
+htmlTable(mx, useViewer=FALSE)
 
 # Now add more interesting options
-htmlTable(mx, 
+htmlTable(mx,
           n.rgroup=c(2), rgroup=c("Nice!"),
           n.cgroup=c(2,1), cgroup=c("First", "Second"))
 
@@ -20,7 +22,7 @@ label(mtcars$mpg) <- "Gas"
 units(mtcars$mpg) <- "Miles/(US) gallon"
 
 label(mtcars$wt) <- "Weight"
-units(mtcars$wt) <- "10^3 kg" # not sure the unit is correct 
+units(mtcars$wt) <- "10^3 kg" # not sure the unit is correct
 
 mtcars$am <- factor(mtcars$am, levels=0:1, labels=c("Automatic", "Manual"))
 label(mtcars$am) <- "Transmission"
@@ -29,26 +31,26 @@ mtcars$gear <- factor(mtcars$gear)
 label(mtcars$gear) <- "Gears"
 
 # Make up some data for making it slightly more interesting
-mtcars$col <- factor(sample(c("red", "black", "silver"), 
+mtcars$col <- factor(sample(c("red", "black", "silver"),
                             size=NROW(mtcars), replace=TRUE))
 label(mtcars$col) <- "Car color"
 
-mpg_data_mean <- getDescriptionStatsBy(mtcars$mpg, mtcars$am, 
+mpg_data_mean <- getDescriptionStatsBy(mtcars$mpg, mtcars$am,
                                        use_units = TRUE, html=TRUE)
-mpg_data_median <- getDescriptionStatsBy(mtcars$mpg, mtcars$am, 
-                                         use_units = TRUE, html=TRUE, 
+mpg_data_median <- getDescriptionStatsBy(mtcars$mpg, mtcars$am,
+                                         use_units = TRUE, html=TRUE,
                                          continuous_fn=describeMedian)
-wt_data <- getDescriptionStatsBy(mtcars$wt, mtcars$am, 
+wt_data <- getDescriptionStatsBy(mtcars$wt, mtcars$am,
                                  use_units = TRUE, html=TRUE)
 
 vars <- rbind(mpg_data_mean, mpg_data_median, wt_data)
 rownames(vars) <- c("Mean (SD)", "Median (IQR)", "Mean (SD)")
 htmlTable(
   vars,
-  caption  = "Continuous & binary variables", 
+  caption  = "Continuous & binary variables",
   n.rgroup = c(2,1), rgroup=c("Gas", "Weight"),
-  n.cgroup = c(2,1), 
-  cgroup   = c(splitLines4Table("Results", 
+  n.cgroup = c(2,1),
+  cgroup   = c(splitLines4Table("Results",
                                 sprintf("n=%d", NROW(mtcars)), html=TRUE),
                ""),
   headings =c(sprintf("%s (SD)", levels(mtcars$am)), "Units"),
@@ -58,10 +60,10 @@ htmlTable(
 ## again with altcol
 htmlTable(
   vars,
-  caption  = "Continuous & binary variables", 
+  caption  = "Continuous & binary variables",
   n.rgroup = c(2,1), rgroup=c("Gas", "Weight"),
-  n.cgroup = c(2,1), 
-  cgroup   = c(splitLines4Table("Results", 
+  n.cgroup = c(2,1),
+  cgroup   = c(splitLines4Table("Results",
                                 sprintf("n=%d", NROW(mtcars)), html=TRUE),
                ""),
   headings =c(sprintf("%s (SD)", levels(mtcars$am)), "Units"),
@@ -77,7 +79,7 @@ describeMedian_minmax <- function(...) describeMedian(..., iqr = FALSE)
 getT1stat <- function(varname, digits = 0) {
   getDescriptionStatsBy(data[ , varname],
                         data$treat,
-                        add_total_col = TRUE, 
+                        add_total_col = TRUE,
                         show_all_values = TRUE,
                         hrzl_prop = FALSE,
                         statistics = FALSE,
@@ -99,7 +101,7 @@ data <- data.frame(age = rpois(100, 50),
 ## table 1 stats
 table_data <- list()
 table_data[['Age']] <- getT1stat('age')
-table_data[['Some categorical<br>&nbsp&nbspvariable']] <- getT1stat('cat_var')
+table_data[['Some categorical<br />&nbsp;&nbsp;variable']] <- getT1stat('cat_var')
 table_data[['Sex']] <- getT1stat('sex')
 table_data[['Race']] <- getT1stat('race')
 
@@ -110,34 +112,48 @@ n.rgroup <- unname(sapply(rgroup, function(x) nrow(table_data[[x]])))
 
 # add a column spanner for the status columns
 cgroup <- c("", "Type of treatment<sup>&dagger;</sup>")
-n.cgroup <- c(1, 3) 
-colnames(output_data) <- 
-  c(paste0('Total<br><font weight = normal; size = 1>n = ',
-           nrow(data), '</font>'),
-    paste0('Treated A<br><font weight = normal; size = 1>n = ',
-           sum(data$treat == 'Treatment A'),'</font>'),
-    paste0('Treatment B&Dagger;<br><font weight = normal; size = 1>n = ',
-           sum(data$treat == 'Treatment B'),'</font>'),
-    paste0('Placebo<br><font weight = normal; size = 1>n = ',
-           sum(data$treat == 'Placebo'),'</font>'))
+n.cgroup <- c(1, 3)
+colnames(output_data) <-
+  c(paste0('Total<br />\n',
+           '<span style="weight = normal; font-size: .6em;">',
+            'n = ', nrow(data),
+           '</span>'),
+    paste0('Treated A<br />\n',
+           '<span style="weight = normal; font-size: .6em;">',
+            'n = ', sum(data$treat == 'Treatment A'),
+           '</span>'),
+    paste0('Treatment B&Dagger;<br />\n',
+           '<span style="weight = normal; font-size: .6em;">',
+            'n = ', sum(data$treat == 'Treatment B'),
+           '</span>'),
+    paste0('Placebo<br />',
+           '<span style="weight = normal; font-size: .6em;">',
+            'n = ', sum(data$treat == 'Placebo'),
+           '</span>'))
 
 
 htmlTable(output_data, align = 'rccc',
-          rgroup = rgroup, n.rgroup = n.rgroup, 
-          rgroupCSSseparator = '', 
+          rgroup = rgroup, n.rgroup = n.rgroup,
+          rgroupCSSseparator = '',
           cgroup = cgroup,
           n.cgroup = n.cgroup,
           tspanner=c("Base", "Other"),
           n.tspanner=c(sum(sapply(table_data, nrow)[1:2]),
                        sum(sapply(table_data, nrow)[3:4])),
-          rowlabel = '', 
+          rowlabel = '',
           ctable = TRUE, # latex-style table lines
-          caption = "Table 1: Patient demographics", 
+          caption = "Table 1: Patient demographics",
           altcol = c('white','lightblue1'),
           tfoot = paste0(
-            '<font size=1>Abbreviations: ECOG, Eastern Cooperative Oncology Group;",
-              " PS, performance score</font><br>',
-            "<font size=1><sup>&dagger;</sup>",
-              "Note 1. Trial groups for a new wonder drug</font><br>",
-            "<font size = 1><sup>&Dagger;</sup>",
-              "Note 2. Twice the dosage of treatment A</font>"))
+            '<span style="font-size: .6em;">',
+              'Abbreviations:',
+              ' ECOG, Eastern Cooperative Oncology Group;',
+              ' PS, performance score',
+            '</span><br />\n',
+            '<span style="font-size: .6em;">',
+              '<sup>&dagger;</sup>',
+              ' Note 1. Trial groups for a new wonder drug</span><br />\n',
+            '<span style="font-size: .6em;">',
+              '<sup>&Dagger;</sup>',
+              ' Note 2. Twice the dosage of treatment A',
+            '</span>'))
